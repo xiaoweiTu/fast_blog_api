c3efdbb0e5a28b402eda70670881881738b73441
xiaowei <13177839316@163.com>
2020-06-16 21:39:57 +0800

    站点信息配置化

--- config/autoload/site_settings.php
+++ config/autoload/site_settings.php
@@ -7,11 +7,11 @@
  */
 
 return [
-    'web_name'    => 'FastBlog',  //网站名称 , 页面左上角展示和title中展示
-    'web_author'  => 'FastBlog',  //文章中展示
-    'web_desc'    => 'a real FastBlog', // 网站描述
-    'web_keyword' => 'laravel,php,hyperf,技术教程,Mysql,Redis,Linux,CentOs',  // 关键词
-    'web_icon'    => 'http://qiniu.txwei.cn/icon-big',  // icon地址
-    'web_record'  => 'FastBlog',    // 备案号
-    'web_who'     => 'PHP', // 站长身份
+    'web_name'    => env('WEB_NAME','FastBlog'),  //网站名称 , 页面左上角展示和title中展示
+    'web_author'  => env('WEB_AUTHOR','FastBlog'),  //文章中展示
+    'web_desc'    => env('WEB_DESC','基于Hyperf搭建高性能博客'), // 网站描述
+    'web_keyword' => env('WEB_KEYWORD','hyperf laravel'),  // 关键词
+    'web_icon'    => env('WEB_ICON',''),  // icon地址
+    'web_record'  => env('WEB_RECORD',''),    // 备案号
+    'web_who'     => env('WEB_WHO'), // 站长身份
 ];
